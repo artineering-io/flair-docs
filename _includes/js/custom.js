cecf2a0487747d7abea4f1b8f47883b7f6b266fa
Santiago Montesdeoca <santiago@artineering.io>
2022-08-30 21:40:31 +0300

    Fixing aio-ui class scaling

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,17 +1,13 @@
 
 jtd.onReady(function(){
   var uiFigures = document.getElementsByClassName('aio-ui');
-  console.log('Running customjs');
   for (var i = 0; i < uiFigures.length; i++) {
     var images = uiFigures[i].getElementsByTagName("img");
     for (var j = 0; j < images.length; j++) {
-      var width = images[i].naturalWidth,
-      height = images[i].naturalHeight;
-
-      console.log(width)
-      console.log(height)
-      images[i].style.maxWidth = (width * 0.8) + "px";
-      images[i].style.maxHeight = (height * 0.8) + "px";
+      var width = images[j].naturalWidth,
+      height = images[j].naturalHeight;
+      images[j].style.maxWidth = (width * 0.7) + "px";
+      images[j].style.maxHeight = (height * 0.7) + "px";
     } 
   }
 })
