ee197cd8f18ea4f940e2d213ab960de94a28bc5c
Santiago Montesdeoca <santiago@artineering.io>
2022-09-15 22:52:13 +0200

    changing from onReady to DOMContentLoaded

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,11 +1,13 @@
 
-jtd.onReady(function(){
+window.addEventListener("DOMContentLoaded", function(){
   var uiFigures = document.getElementsByClassName('aio-ui');
   for (var i = 0; i < uiFigures.length; i++) {
     var images = uiFigures[i].getElementsByTagName("img");
     for (var j = 0; j < images.length; j++) {
       var width = images[j].naturalWidth,
       height = images[j].naturalHeight;
+      console.log(width)
+      console.log(height)
       images[j].style.maxWidth = (width * 0.7) + "px";
       images[j].style.maxHeight = (height * 0.7) + "px";
     } 
